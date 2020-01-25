bfbd552c455744a14fc12936dfd29f5c6657b4c6
Chandan Jain <45136369+chandanjainn@users.noreply.github.com>
2020-01-25 22:24:20 +0530

    Create NumberOfOccurences.js

---
+++ Others/NumberOfOccurences.js
@@ -0,0 +1,11 @@
+ var string = 'This is a string',
+    searchFor = 'is',
+    count = 0,
+    pos = string.indexOf(searchFor);
+
+while (pos > -1) {
+    ++count;
+    pos = string.indexOf(searchFor, ++pos);
+}
+
+console.log(count); 
